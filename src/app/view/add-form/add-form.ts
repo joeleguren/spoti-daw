c2d819efedcb53a3a8ce863a4828492995f7d39b
Joel Eguren <joeleguren7@gmail.com>
2025-11-01 19:35:09 +0100

    Add components

---
+++ src/app/view/add-form/add-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-form',
+  imports: [],
+  templateUrl: './add-form.html',
+  styleUrl: './add-form.css',
+})
+export class AddForm {
+
+}
---
+++ src/app/view/music-list/music-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-music-list',
+  imports: [],
+  templateUrl: './music-list.html',
+  styleUrl: './music-list.css',
+})
+export class MusicList {
+
+}
